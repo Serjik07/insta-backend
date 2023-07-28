e70604950a6b99922ae6f5d1c730b93fb5f93eca
Serjik07 <serjik.h1@tumo.org>
2023-07-28 22:56:36 +0400

    not error

--- instagram/main/views.py
+++ instagram/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm
+from .models import Login
 # Create your views here.
+def esim(requset):
+    log = Login.objects.all()
+    return render(requset, "main/esim.html",{"login": log})
 
 def home(request):
     form = LoginForm()
